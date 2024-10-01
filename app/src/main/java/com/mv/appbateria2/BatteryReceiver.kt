b57488e847b3313acf062b7e0539ff42ccaeff3b
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-10-01 02:45:30 -0500

    Actualizando el TextView usando un Intent adicional en la nueva clase

--- app/src/main/java/com/mv/appbateria2/BatteryReceiver.kt
+++ app/src/main/java/com/mv/appbateria2/BatteryReceiver.kt
@@ -3,9 +3,16 @@ package com.mv.appbateria2
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.BatteryManager
+import android.util.Log
 
 class BatteryReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        
+        val level = intent?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
+        Log.d("BatteryReceiver", "Nivel de la batería: $level%")
+
+        val updateIntent = Intent("com.example.UPDATE_BATTERY")
+        updateIntent.putExtra("level", level)
+        context?.sendBroadcast(updateIntent)
     }
 }
