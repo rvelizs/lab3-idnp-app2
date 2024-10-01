acfb983c65caf958783bd56d869a41199333befa
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-10-01 03:44:07 -0500

    PendingIntent creado

--- app/src/main/java/com/mv/appbateria2/MainActivity.kt
+++ app/src/main/java/com/mv/appbateria2/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.mv.appbateria2
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -13,8 +15,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-        
+
+        texto = findViewById(R.id.txtNivel)
+        batteryReceiver = BatteryReceiver()
+
+        // Crea un Intent para BatteryReceiver
+        val batteryIntent = Intent(this, BatteryReceiver::class.java)
+
+        // Crea un PendingIntent que será usado para lanzar el BroadcastReceiver
+        val pendingIntent = PendingIntent.getBroadcast(
+            this,
+            0,
+            batteryIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
     }
 }
