f7f4c01ed0ec953ba6368da953e550c32908b20a
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-10-01 03:46:45 -0500

    Broadcast de actualizar nivel de batería listo

--- app/src/main/java/com/mv/appbateria2/MainActivity.kt
+++ app/src/main/java/com/mv/appbateria2/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.mv.appbateria2
 
 import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -13,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var texto: TextView
     private lateinit var batteryReceiver: BatteryReceiver
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,5 +37,13 @@ class MainActivity : AppCompatActivity() {
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
+        // Registrar el Broadcast para actualizar el nivel de la batería
+        val batteryStatusIntentFilter = IntentFilter("com.example.UPDATE_BATTERY")
+        registerReceiver(object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val level = intent?.getIntExtra("level", -1) ?: -1
+                texto.text = "$level%"
+            }
+        }, batteryStatusIntentFilter, RECEIVER_NOT_EXPORTED)
     }
 }
