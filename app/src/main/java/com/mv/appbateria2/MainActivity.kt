1daebcaa65974418757ec5ea7345141c050ca76e
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-10-01 03:47:48 -0500

    Métodos onResume y onPause

--- app/src/main/java/com/mv/appbateria2/MainActivity.kt
+++ app/src/main/java/com/mv/appbateria2/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
@@ -45,5 +46,20 @@ class MainActivity : AppCompatActivity() {
                 texto.text = "$level%"
             }
         }, batteryStatusIntentFilter, RECEIVER_NOT_EXPORTED)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Crear filtro de intención para el estado de la batería
+        val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        sendBroadcast(Intent(this, BatteryReceiver::class.java))
+        Log.d("MainActivity", "BroadcastReceiver registrado satisfactoriamente")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // Desregistrar el receptor cuando la app no está visible
+        unregisterReceiver(batteryReceiver)
+        Log.d("MainActivity", "BroadcastReceiver desregistrado satisfactoriamente")
     }
 }
