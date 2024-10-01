377982c59b0062adb6ea0397fc63b76a18f120b0
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-10-01 02:43:05 -0500

    función onReceive en la nueva clase (BatteryReceiver)

---
+++ app/src/main/java/com/mv/appbateria2/BatteryReceiver.kt
@@ -0,0 +1,11 @@
+package com.mv.appbateria2
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BatteryReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        
+    }
+}
